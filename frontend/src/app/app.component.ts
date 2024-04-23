56ac8e2ecae2d1a7180ccbf4b9eea7ce55ecae3c
kiril <kirill.301522@gmail.com>
2024-04-24 00:58:00 +1000

    __init__

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {DomSanitizer} from "@angular/platform-browser";
+import {MatIconRegistry} from "@angular/material/icon";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+  ) {
+    this.supplementMatIconRegistry();
+  }
+
+  public supplementMatIconRegistry() {
+    this.matIconRegistry.addSvgIcon(
+      `pixel-icon`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl("/assets/icons/pixel-logo.svg")
+    ).addSvgIcon(
+      `search-loupe`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl("/assets/icons/search-loupe.svg")
+    )
+  }
 }
