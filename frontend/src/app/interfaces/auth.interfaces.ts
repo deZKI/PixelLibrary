a8dca5ad3e124815c4370d18c9007bc51aa7c3c2
kiril <kirill.301522@gmail.com>
2024-04-28 02:06:46 +1000

    auth service

--- frontend/src/app/interfaces/auth.interfaces.ts
+++ frontend/src/app/interfaces/auth.interfaces.ts
@@ -1,5 +1,22 @@
 import {Email} from "./shared.interfaces";
 
-export interface RegistrationForm extends Email {
+export interface authForm extends Email {
   password: string
 }
+
+export interface TokenRefresh {
+  refresh: string
+}
+
+export interface TokenAccess {
+  access: string
+}
+
+export interface TokenVerify {
+  token: string
+}
+
+export interface TokenPairObtain extends TokenAccess, TokenRefresh {
+
+}
+
--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import {HttpClient} from "@angular/common/http";
-import {RegistrationForm} from "../interfaces/auth.interfaces";
+import {authForm, TokenAccess, TokenPairObtain, TokenRefresh} from "../interfaces/auth.interfaces";
 import {Observable} from "rxjs";
 import {Email} from "../interfaces/shared.interfaces";
 import {environment} from "../../enviroments/environment";
@@ -12,13 +12,23 @@ import {Injectable} from "@angular/core";
 export class AuthService {
 
   registrationURL = 'auth/registration'
+  loginURL = 'auth/login'
+  refreshURL = 'token/refresh'
 
   constructor(
     private http: HttpClient
   ) {
   }
 
-  registration(registrationForm: RegistrationForm): Observable<Email> {
-    return this.http.post<Email>(`${environment.url}/${environment.api}/${this.registrationURL}/`, registrationForm)
+  registration(authForm: authForm): Observable<Email> {
+    return this.http.post<Email>(`${environment.url}/${environment.api}/${this.registrationURL}/`, authForm)
+  }
+
+  login(authForm: authForm): Observable<TokenPairObtain> {
+    return this.http.post<TokenPairObtain>(`${environment.url}/${environment.api}/${this.loginURL}/`, authForm)
+  }
+
+  refresh(refreshToken: TokenRefresh) {
+    return this.http.post<TokenAccess>(`${environment.url}/${environment.api}/${this.refreshURL}/`, refreshToken)
   }
 }
