59dcc6d4317d501e0c477e94747eaa2b2ae9bb69
kiril <kirill.301522@gmail.com>
2024-05-15 00:14:03 +1000

    profile

--- core/users/admin.py
+++ core/users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from users.models import AuthComment, BookComment, Users
 
@@ -7,9 +9,43 @@ admin.site.unregister(Group)
 
 
 @admin.register(Users)
-class UsersAdmin(admin.ModelAdmin):
+class UsersAdmin(UserAdmin):
     """ Custom User Admin """
-    pass
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": (
+            "first_name", "last_name", "patronymic", "phone_number", "birthday_date", "balance", "telegram_chat_id")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("first_name", "last_name", "email")
+    ordering = ("email",)
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
 
 
 @admin.register(BookComment)
