8b304cadea4bb6ca9cf30fa420340001bbbf1935
kiril <kirill.301522@gmail.com>
2024-05-19 22:18:59 +1000

    docker

--- core/entrypoint.sh
+++ core/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # Проверка на доступность базы данных перед выполнением миграций
-# Это особенно важно для продакшен сред, где база данных может запускаться независимо и не всегда доступна сразу
 echo "Waiting for PostgreSQL to start..."
 while ! nc -z postgres_db 5432; do
   sleep 0.1
@@ -14,12 +13,11 @@ python manage.py migrate --noinput
 # Сбор статических файлов
 python manage.py collectstatic --noinput --clear
 
-# fixtures users
-python manage.py loaddata fixtures/users.json
-
 # Запуск Gunicorn или любого другого WSGI сервера, который вы используете
 gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers=7
+
 # Start server
 echo "Starting server"
+
 # run the container CMD
 exec "$@"
